ab6a1dd149f55f2c317567f9a16d44d7a5e3312d
Pierrebory <pier.bory@gmail.com>
2017-11-09 09:41:05 +0100

    structure html : un message explicite

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Titre de la page</title>
+  
+</head>
+<body>
+    <p> BONJOUR</p>
+</body>
+</html>
